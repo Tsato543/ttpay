d2caae90cc8c9defd5f76f0bf395081116e8fc85
Tsato543 <tsato4539@gmail.com>
2026-01-27 00:29:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Up1 from "./pages/Up1";
 import Up2 from "./pages/Up2";
@@ -11,6 +12,12 @@ import Up4 from "./pages/Up4";
 import Up5 from "./pages/Up5";
 import Up6 from "./pages/Up6";
 import NotFound from "./pages/NotFound";
+
+// PRESSELL (nomes reais do seu projeto)
+import Presell from "./pages/Presell";
+import Loading from "./pages/Loading";
+import StepAge from "./pages/StepAge";
+import StepName from "./pages/StepName";
 
 const queryClient = new QueryClient();
 
@@ -21,14 +28,24 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+
+          {/* 👉 PRESSELL AGORA É A HOME */}
+          <Route path="/" element={<Presell />} />
+          <Route path="/loading" element={<Loading />} />
+          <Route path="/idade" element={<StepAge />} />
+          <Route path="/nome" element={<StepName />} />
+
+          {/* 👉 SITE PRINCIPAL */}
+          <Route path="/home" element={<Index />} />
+
+          {/* Ups */}
           <Route path="/up1" element={<Up1 />} />
           <Route path="/up2" element={<Up2 />} />
           <Route path="/up3" element={<Up3 />} />
           <Route path="/up4" element={<Up4 />} />
           <Route path="/up5" element={<Up5 />} />
           <Route path="/up6" element={<Up6 />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
