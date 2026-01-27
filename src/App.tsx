b4f3b606d82a343d94f63ea0ac7d22ed6a8a6466
Tsato543 <tsato4539@gmail.com>
2026-01-27 00:33:40 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Presell from "./pages/Presell";
+import StepAge from "./pages/StepAge";
+import StepName from "./pages/StepName";
+import Loading from "./pages/Loading";
+
 import Index from "./pages/Index";
 import Up1 from "./pages/Up1";
 import Up2 from "./pages/Up2";
@@ -12,12 +17,6 @@ import Up4 from "./pages/Up4";
 import Up5 from "./pages/Up5";
 import Up6 from "./pages/Up6";
 import NotFound from "./pages/NotFound";
-
-// PRESSELL (nomes reais do seu projeto)
-import Presell from "./pages/Presell";
-import Loading from "./pages/Loading";
-import StepAge from "./pages/StepAge";
-import StepName from "./pages/StepName";
 
 const queryClient = new QueryClient();
 
@@ -28,17 +27,16 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-
-          {/* 👉 PRESSELL AGORA É A HOME */}
+          {/* FUNIL PRESSELL */}
           <Route path="/" element={<Presell />} />
+          <Route path="/stepage" element={<StepAge />} />
+          <Route path="/stepname" element={<StepName />} />
           <Route path="/loading" element={<Loading />} />
-          <Route path="/idade" element={<StepAge />} />
-          <Route path="/nome" element={<StepName />} />
 
-          {/* 👉 SITE PRINCIPAL */}
+          {/* SITE PRINCIPAL */}
           <Route path="/home" element={<Index />} />
 
-          {/* Ups */}
+          {/* UPS */}
           <Route path="/up1" element={<Up1 />} />
           <Route path="/up2" element={<Up2 />} />
           <Route path="/up3" element={<Up3 />} />
